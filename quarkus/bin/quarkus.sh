44e7310848372c24a64cbe794becf3e579b0abf5
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2020-06-30 09:28:05 +0300

    quarkus bootstrap added

---
+++ quarkus/bin/quarkus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARTIFACT_ID=${ARTIFACT_ID:-"quarkus-demo"}
+BINDING=${BINDING:="/hello"}
+EXTENSIONS=${EXTENSIONS:-"quarkus-kubernetes-client,resteasy-jsonb,resteasy-mutiny"}
+#EXTENSIONS="${EXTENSIONS:-quarkus-container-image-s2i,quarkus-kubernetes-client,quarkus-openshift,resteasy-jsonb,resteasy-mutiny"}
+GROUP_ID=${GROUP_ID:-"com.redhat"}
+PACKAGE=${PACKAGE:-"codeready."}
+RESOURCE_NAME=${RESOURCE_NAME:-"ReactiveGreetingResource"}
+
+mvn io.quarkus:quarkus-maven-plugin:1.5.2.Final:create \
+    -DprojectGroupId="$GROUP_ID" \
+    -DprojectArtifactId="$ARTIFACT_ID" \
+    -DclassName="${GROUP_ID}.${PACKAGE}${RESOURCE_NAME}" \
+    $([ -z "$BINDING" ] || echo "-Dpath='$BINDING'") \
+    -Dextensions="$EXTENSIONS"
